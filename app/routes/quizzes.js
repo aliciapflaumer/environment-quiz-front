5fc12ad7ea78514f0233b1b9e3293551ca2ecc6a
aliciapflaumer <alpfla7@gmail.com>
2017-12-08 18:41:47 -0500

    Create quiz model
    - Used `ember generate model quiz title:string hidden:boolean`
    to create a model for ember app to represent the quiz data from the API.

--- app/routes/quizzes.js
+++ app/routes/quizzes.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model () {
+     return this.get('store').findAll('quiz');
+   },
+   actions: {
+     createQuiz(quiz) {
+       let newQuiz = this.get('store').createRecord('quiz', quiz);
+       newQuiz.save();
+     },
+     deleteQuiz(quiz) {
+       quiz.destroyRecord();
+     },
+     updateQuiz(quiz, title) {
+       console.log(quiz);
+       console.log(title);
+       quiz.set('title', title)
+     }
+   }
 });

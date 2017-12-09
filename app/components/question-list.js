cdba92c6894dbb3efeaf178dd9a9036ed34cfae9
aliciapflaumer <alpfla7@gmail.com>
2017-12-09 14:14:16 -0500

    Update question-form component to send action up
    - Send submit action and question data up to question-list component
    and then to quiz route.
    
    - Could not use `.createRecord` or `.save` as expected, will need
    to customize Ember's AJAX call.

--- app/components/question-list.js
+++ app/components/question-list.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  classNames: ['environment-quiz-api'],
+  classNameBindings: ['quizDetailHidden'],
+  quizDetailHidden: false,
+  actions: {
+    createQuestion(question) {
+      return this.sendAction('createQuestion', question);
+    }
+  },
 });

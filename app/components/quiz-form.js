61902126eb115f3956bc87fc29794fe09582414f
aliciapflaumer <alpfla7@gmail.com>
2017-12-08 21:48:22 -0500

    Update quiz-form component to bind input data
    - Define a `newQuiz` object in our component to store the default
    quiz data for the form.
    
    - Bind the `.title` to the `input` in quiz-form.hbs

--- app/components/quiz-form.js
+++ app/components/quiz-form.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  newQuiz: {
+    title: null,
+    hidden: false
+  },
+  actions: {
+    createQuiz() {
+      console.log(this.get('newQuiz'));
+    }
+  }
 });

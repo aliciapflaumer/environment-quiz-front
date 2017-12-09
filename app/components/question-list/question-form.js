5082a47f5a9842907ae3864b701807213a32b403
aliciapflaumer <alpfla7@gmail.com>
2017-12-09 13:30:35 -0500

    Update question-form to bind input data
    - Bind newQuestion.content to the input using
    `value=newQuestion.content`

--- app/components/question-list/question-form.js
+++ app/components/question-list/question-form.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newQuestion: {
+    content: null,
+    done: false
+  },
   actions: {
     createQuestion() {
-      console.log('submitted');
+      // console.log('submitted');
+      console.log(this.get('newQuestion'));
     }
   }
 });

2de6e06e35cc8fd370d1c94470c33ec6168747d4
aliciapflaumer <alpfla7@gmail.com>
2017-12-27 13:27:50 -0500

    Generated service store
    Used `ember generate service store` to generate service store

--- app/routes/quizzes/quiz.js
+++ app/routes/quizzes/quiz.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params){
-    return Ember.RSVP.hash({
-      quiz: this.get('store').findRecord('quiz', params.quiz_id)
-    });
-  }
+  model(){}
 });
